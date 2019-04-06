e7a92186db6aaba02d269057e652ebf4ed85dffc
max344 <0maxsimka0@gmail.com>
2019-04-06 14:28:14 +0300

    its work

---
+++ text_redaktor/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace text_redaktor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       // string fileName;
+        string filePath;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath=openFileDialog.FileName;
+                
+                tbMain.Text = File.ReadAllText(openFileDialog.FileName, Encoding.GetEncoding(1251));
+            }
+        }
+        
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        
+        private void SaveText_Click(object sender, RoutedEventArgs e)
+        {
+           
+            File.AppendAllText(filePath, tbMain.Text);
+        }
+    }
+}
